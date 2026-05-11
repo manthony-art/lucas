df3ac668bb589615c6cd5836fab54902df809050
manthony-art <manthony@jsps.ac.ke>
2026-05-11 20:39:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-from flask import Flask, render_template_string
-import os
 
-app = Flask(__name__)
-app.secret_key = os.urandom(24)
-
-@app.route('/')
-def index():
-    return render_template_string(HTML)
-
-if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
